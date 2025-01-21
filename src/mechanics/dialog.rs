8ce33f504fc2cf2c9e19474b5eeb449a972d23e7
Jolvera2001 <jolvera6@stedwards.edu>
2025-01-20 22:56:26 -0600

    added fetch_dialog_data system

--- src/mechanics/dialog.rs
+++ src/mechanics/dialog.rs
@@ -9,13 +9,19 @@ impl Plugin for DialogPlugin {
     fn build(&self, app: &mut App) {
         app
         .add_plugins(RonAssetPlugin::<DialogAsset>::new(&["dialog_test.ron"]))
+        .add_event::<DialogEvent>()
         .add_systems(Update, fetch_dialog_data);
     }
 }
 
-#[derive(Asset, TypePath, Debug, Deserialize)]
+#[derive(Asset, TypePath, Debug, Deserialize, Clone)]
 pub struct DialogAsset {
     lines: Vec<String>,
+}
+
+#[derive(Event)]
+pub enum DialogEvent{
+    DialogData(DialogAsset)
 }
 
 // marker
@@ -28,10 +34,18 @@ pub struct DialogData {
 }
 
 fn fetch_dialog_data(
-    dialog_event: EventReader<InteractType>,
-    dialog_entity: Query<(&DialogData, Entity), With<DialogEntity>>,
+    mut dialog_event: EventReader<InteractType>,
+    mut event_writer: EventWriter<DialogEvent>,
+    dialog_query: Query<(&DialogData, Entity), With<DialogEntity>>,
+    dialog_assets: Res<Assets<DialogAsset>>,
 ) {
     for event in dialog_event.read() {
-        if event.contains()
+        if let InteractType::Dialog(entity) = event {
+            if let Ok((dialog_data, _)) = dialog_query.get(*entity) {
+                if let Some(asset) = dialog_assets.get(&dialog_data.dialog_file) {
+                    event_writer.send(DialogEvent::DialogData(asset.clone()));
+                }
+            }
+        }
     }
 }
