edc93f94eb39a3650c0491580c6fcc5421098e2a
Jolvera2001 <jolvera6@stedwards.edu>
2025-01-20 23:21:16 -0600

    got dialog box going

--- src/gui_plugin.rs
+++ src/gui_plugin.rs
@@ -1,6 +1,8 @@
 use bevy::prelude::*;
 use bevy::app::AppExit;
-use bevy_egui::{egui, EguiContexts};
+use bevy_egui::{egui::{self, Color32, RichText}, EguiContexts};
+
+use crate::mechanics::dialog::DialogEvent;
 
 pub struct GuiPlugin;
 
@@ -11,19 +13,24 @@ impl Plugin for GuiPlugin {
         .add_systems(Update, (
             handle_pause, 
             handle_game_events,
-            render_pause_menu
+            render_pause_menu,
+            render_dialog_box,
         ));
     }
 }
 
 #[derive(Event)]
 enum GameEvent {
-    TogglePause
+    TogglePause,
+    StartDialog,
+    EndDialog,
 }
 
 #[derive(Component)]
 pub struct GameState {
-    pub paused: bool
+    pub paused: bool,
+    pub in_dialog: bool,
+    pub current_dialog_line: usize,
 }
 
 #[derive(Component)]
@@ -31,7 +38,11 @@ pub struct GameStateMarker;
 
 fn setup_gui_plugin(mut commands: Commands) {
     commands.spawn((
-        GameState { paused: false },
+        GameState { 
+            paused: false,
+            in_dialog: false,
+            current_dialog_line: 0,
+        },
         GameStateMarker,
     ));
 }
@@ -55,7 +66,13 @@ fn handle_game_events(
                 GameEvent::TogglePause => {
                     game_state.paused = !game_state.paused;
                     println!("Game paused: {}", game_state.paused);
-                }     
+                },
+                GameEvent::StartDialog => {
+                    game_state.in_dialog = !game_state.in_dialog;
+                },
+                GameEvent::EndDialog => {
+                    game_state.in_dialog = !game_state.in_dialog;
+                }
             }
         }
     }
@@ -80,6 +97,51 @@ fn render_pause_menu(
                     exit.send(AppExit::Success);
                 }
             });
+        }
+    }
+}
+
+fn render_dialog_box(
+    mut contexts: EguiContexts,
+    mut event_writer: EventWriter<GameEvent>,
+    mut dialog_event: EventReader<DialogEvent>,
+    mut game_state: Query<&mut GameState>,
+    input: Res<ButtonInput<KeyCode>>,
+) {
+    if let Ok(mut state) = game_state.get_single_mut() {
+        for event in dialog_event.read() {
+            if let DialogEvent::DialogData(dialog_asset) = event {
+                state.in_dialog = true;
+                state.current_dialog_line = 0;
+            }
+        }
+
+        if state.in_dialog {
+            egui::Window::new("Dialog")
+                .anchor(egui::Align2::CENTER_BOTTOM, [0.0, -30.0])
+                .resizable(false)
+                .collapsible(false)
+                .fixed_size([600.0, 150.0])
+                .show(contexts.ctx_mut(), |ui| {
+                    ui.vertical_centered(|ui| {
+                        ui.label("Character Name");
+                        ui.add_space(10.0);
+                        ui.label(RichText::new("Dialog text here")
+                            .size(16.0)
+                            .family(egui::FontFamily::Proportional)
+                        );
+    
+                        ui.add_space(10.0);
+                        ui.label(RichText::new("[Space] Continue")
+                                .size(12.0)
+                                .color(Color32::LIGHT_GRAY));
+                    });
+                });
+
+            if input.just_pressed(KeyCode::Space) {
+                state.current_dialog_line += 1;
+                // add logic for reaching the end of dialog
+            }
         }
     }
 }
